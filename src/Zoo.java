4da5de05815de064bc7878173bdd785fa6ee28b2
Noe <noemotamedi@students.berkeley.net>
2018-10-15 13:38:03 -0700

    Initial commit

---
+++ src/Zoo.java
@@ -0,0 +1,15 @@
+class Zoo{
+
+    public static void main(String[] args) {
+        //either make a new instance of a zoo
+        //to call the non-static sleep method....
+        Tiger tigger = new Tiger("Tigger");
+        tigger.eat( "meat");
+        tigger.eat( "kibble");
+        tigger.sleep();
+    }
+
+
+
+
+}
